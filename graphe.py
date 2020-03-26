d7d0a1de7e8db246d0276e473f00a18b2ffff114
Pierre Boislève <pierre.boisleve@gmail.com>
2020-03-26 08:52:35 +0100

    Initial commit

---
+++ graphe.py
@@ -0,0 +1,6 @@
+class Graphe:
+        """Class Test."""
+        _graph_id = 1
+        _nbr_nodes = 1
+        _nodes_dictionary = 1
+        _links_dictionary = 1
---
+++ lien.py
@@ -0,0 +1,12 @@
+class Lien:
+    """Class Test."""
+    __id = 1
+    __nodes = 1
+    __distance = 1
+
+    def __init__(self, node1, node2):
+        """Initialize the value."""
+        self.__value = 1
+        Lien.__id += 1
+        Lien.__nodes += 1
+        Lien.__distance += 1
