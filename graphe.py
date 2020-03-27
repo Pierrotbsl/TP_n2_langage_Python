5d5a8b58a718af11604e94ba581403bc2f941b5c
Pierre Boislève <pierre.boisleve@gmail.com>
2020-03-27 18:09:32 +0100

    initial commit

--- graphe.py
+++ graphe.py
@@ -1,6 +1,21 @@
 class Graphe:
-        """Class Test."""
-        _graph_id = 1
-        _nbr_nodes = 1
-        _nodes_dictionary = 1
-        _links_dictionary = 1
+
+    def __init__(self, id_, nbrNodes, dictionaryLink):
+        self._id = id_
+        self._nbrNodes = nbrNodes
+        self._nodeDictionary = 0
+        self._DictionaryLink = dictionaryLink
+
+    def getNbNodes(self):
+        return self._nodeDictionary
+
+    def addNode(self, node):
+        self._nbrNodes += 1
+        #self._dictionnaireNoeuds.append(node)
+
+
+    def getNextNode(self, idNode):
+        return
+
+    def __str__(self):
+        print("Liste des noeuds & liens du graphe")
