625ac406c0dfa6f44ef29c59164ea8e9b2ebf28c
Pierre Boislève <pierre.boisleve@gmail.com>
2020-03-27 18:28:01 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-def createGraph(self, idGraph, ):
+import csv
+
+def createGraph(self, idGraph, filename):
+    file = open(filename, "rb")
+    try:
+        reader = csv.reader(file)
+        for row in reader:
+            print row
+    finally:
+        file.close()
